8f12b311c01a7f5002fb2ed232d65f2bde39269b
Tejaswi Dabbir <tejaswi.kumar97@gmail.com>
2022-10-29 12:33:44 -0500

    Adding bootstrap cdn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="col-lg-12">
+      <div class="row">
+        <div class="col-lg-3">
+          Card1
+        </div>
+        <div class="col-lg-3">
+          Card2
+        </div>
+        <div class="col-lg-3">
+          Card3
+        </div>
+        <div class="col-lg-3">
+          Card4
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
