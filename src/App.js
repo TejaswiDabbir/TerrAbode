4932116f53d90fe91f1fc0e08cb848013293f5f9
Tejaswi Dabbir <tejaswi.kumar97@gmail.com>
2022-10-29 12:59:51 -0500

    Creating components folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
+import Card from './components/card';
 
 function App() {
   return (
     <div class="col-lg-12">
       <div class="row">
-        <div class="col-lg-3">
-          Card1
-        </div>
-        <div class="col-lg-3">
-          Card2
-        </div>
-        <div class="col-lg-3">
-          Card3
-        </div>
-        <div class="col-lg-3">
-          Card4
-        </div>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
       </div>
     </div>
   )
---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div class="col-lg-3">
+      Card Component
+      {/* CarouselComponent */}
+      {/* CardDataComponent */}
+    </div>
+  )
+};
+
+export default Card
